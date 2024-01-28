83455a2e6efc2842a4cf7ef681e44686ed5441d4
Mwesigye Jordan Alvin <jalvinmwea23@gmail.com>
2024-01-28 13:31:21 +0300

    Bluetooth operations but with major errors - min sdk changed to 21

--- app/src/main/java/com/example/myapplicationtrial/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtrial/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.myapplicationtrial;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 10000;
+    private LeDeviceListAdapter leDeviceListAdapter;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private boolean scanning;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothManager bluetoothManager = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            bluetoothManager = getSystemService(BluetoothManager.class);
+        }
+        BluetoothAdapter bluetoothAdapter = null;
+
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        leDeviceListAdapter = new LeDeviceListAdapter();
+        handler = new Handler();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else{
+            bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        }
     }
+    // Device scan callback.
+    private ScanCallback leScanCallback = new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    leDeviceListAdapter.addDevice(result.getDevice());
+                    leDeviceListAdapter.notifyDataSetChanged();
+                }
+            };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                scanLeDevice();
+                // Bluetooth was successfully enabled by the user
+            } else {
+                // The user declined or there was an error enabling Bluetooth
+                // You may want to handle this case, for example, show a message to the user.
+            }
+        }
+    }
+    private void scanLeDevice() {
+        if (!scanning) {
+            // Stops scanning after a predefined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    bluetoothLeScanner.stopScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        } else {
+            scanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
+
 }
