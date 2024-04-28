46ba5695ec93638911d8a967791c92c7c80fc2c5
samueltexa <woolardsamuels@gmail.com>
2024-04-28 23:10:38 +0300

    Scan Result

--- EazyBLE/src/main/java/eazyble/ScannerBuilder/ScannerResultsBuilder.java
+++ EazyBLE/src/main/java/eazyble/ScannerBuilder/ScannerResultsBuilder.java
@@ -1,4 +1,4 @@
-package eazyble.Interfaces.Scanner;
+package eazyble.ScannerBuilder;
 
 
 // data model to hold information about scanned devices
@@ -7,12 +7,14 @@ public class ScannerResultsBuilder {
     private int rssi;
     private String uuid;
     private String macAddress;
+    private String proximity;
 
-    public ScannerResultsBuilder(String deviceName, int rssi, String uuid, String macAddress) {
+    public ScannerResultsBuilder(String deviceName, int rssi, String uuid, String macAddress, String proximity) {
         this.deviceName = deviceName;
         this.rssi = rssi;
         this.uuid = uuid;
         this.macAddress = macAddress;
+        this.proximity = proximity;
     }
 
     public String getDeviceName() {
@@ -28,5 +30,8 @@ public class ScannerResultsBuilder {
     }
     public String getMacAddress() {
         return macAddress;
+    }
+    public String proximity() {
+        return proximity;
     }
 }
