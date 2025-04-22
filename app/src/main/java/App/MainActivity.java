803849f383ae6019872301f5f82ca89ad658fb13
samueltexa <woolardsamuels@gmail.com>
2025-04-21 15:14:38 -1100

    remove sensitivity on fragments

--- app/src/main/java/App/MainActivity.java
+++ app/src/main/java/App/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.navigation.Navigation;
 import com.example.blemasterslave.databinding.ActivityMainBinding;
 import com.example.blemasterslave.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
 import eazyble.Permissions;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,15 +29,19 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnItemSelectedListener(item -> {
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-            if (item.getItemId() == R.id.scanner) {
+            int itemId = item.getItemId();
+            int currentDestId = Objects.requireNonNull(navController.getCurrentDestination()).getId();
+
+            if (itemId == R.id.scanner && currentDestId != R.id.scanner) {
                 navController.navigate(R.id.scanner);
                 return true;
-            } else if (item.getItemId() == R.id.advertiser) {
+            } else if (itemId == R.id.advertiser && currentDestId != R.id.advertiser) {
                 navController.navigate(R.id.advertiser);
                 return true;
             }
             return false;
         });
+
     }
     // Results returned from bluetooth decision after requesting permission
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -47,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_BLUETOOTH_SCAN_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed with advertising
+                // Permission granted, proceed with sink
                 Toast.makeText(this, "Scanning permission granted", Toast.LENGTH_SHORT).show();
             } else {
                 // Permission denied, handle accordingly
@@ -60,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                     Permissions.grantedPermission(this);
                     //Location permission denied
                 } else {
-//                    Toast.makeText(this, "Advertisings permission denied", Toast.LENGTH_SHORT).show();
                     Permissions.deniedPermission(this);
                 }
             }
